7968242e759ddf807723236bc1d0c1976339b821
MarceloTorres88 <russo1988torres@gmail.com>
2020-11-11 14:55:01 -0300

    Test Observatorio sin problemas

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/RegionalBloc.kt
+++
@@ -1,3 +0,0 @@
-package ar.edu.unahur.obj2.impostoresPaises
-
-class RegionalBloc (val acronym: String, val name: String)
--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/RegionalBlocTest.kt
+++
@@ -1,9 +0,0 @@
-package ar.edu.unahur.obj2.impostoresPaises
-
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
-
-class RegionalBlocTest : FunSpec({
-
-
-})
