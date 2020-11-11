23fd55d92236ef84e991872f0d57e7cc5c46d271
MarceloTorres88 <russo1988torres@gmail.com>
2020-11-11 14:52:34 -0300

    comenzando con el testeo

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Pais.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Pais.kt
@@ -7,8 +7,8 @@ interface Pais {
     val region: String
     val population: Int
     val borders: List<String>
-    val languages: List<Lenguages>
-    val regionalBlocs: List<regionalBlocs>
+    val languages: List<Language>
+    val regionalBlocs: List<RegionalBloc>
 
     fun esLimitrofeCon(pais: Pais) = this.borders.contains(pais.alpha3Code)
 
@@ -33,9 +33,9 @@ interface Pais {
     }
 }
 
-class Lenguages (val name : String)
+class Languages (val name : String)
 
-class regionalBlocs (val acronym: String, val name: String)
+class RegionalBlocs (val acronym: String, val name: String)
 
 object Argentina : Pais{
     override val name = "Argentina"
@@ -44,8 +44,8 @@ object Argentina : Pais{
     override val region = "Americas"
     override val population = 43590400
     override val borders = listOf<String>("BOL", "BRA", "CHL", "PRY", "URY")
-    override val languages = listOf<Lenguages>(Lenguages("Spanish"), Lenguages("Guaraní"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("USAN","Union of South American Nations"))
+    override val languages = listOf<Language>(Language("Spanish"), Language("Guaraní"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("USAN","Union of South American Nations"))
 }
 
 object Brasil : Pais{
@@ -55,8 +55,8 @@ object Brasil : Pais{
     override val region = "Americas"
     override val population = 206135893
     override val borders = listOf<String>("ARG","BOL","COL", "GUF", "GUY","PRY","PER","SUR","URY","VEN")
-    override val languages = listOf<Lenguages>(Lenguages("Portuguese"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("USAN","Union of South American Nations"))
+    override val languages = listOf<Language>(Language("Portuguese"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("USAN","Union of South American Nations"))
 }
 
 object Peru : Pais{
@@ -66,8 +66,8 @@ object Peru : Pais{
     override val region = "Americas"
     override val population = 31488700
     override val borders = listOf<String>("BOL", "BRA", "CHL", "COL", "ECU")
-    override val languages = listOf<Lenguages>(Lenguages("Spanish"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("PA","Pacific Alliance"),regionalBlocs("USAN","Union of South American Nations"))
+    override val languages = listOf<Language>(Language("Spanish"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("PA","Pacific Alliance"),RegionalBloc("USAN","Union of South American Nations"))
 }
 
 object Bolivia : Pais{
@@ -77,8 +77,8 @@ object Bolivia : Pais{
     override val region = "Americas"
     override val population = 10985059
     override val borders = listOf<String>("ARG","BRA","CHL","PRY","PER")
-    override val languages = listOf<Lenguages>(Lenguages("Spanish"), Lenguages("Aymara"), Lenguages("Quechua"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("USAN","Union of South American Nations"))
+    override val languages = listOf<Language>(Language("Spanish"), Language("Aymara"), Language("Quechua"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("USAN","Union of South American Nations"))
 }
 
 object España : Pais{
@@ -88,8 +88,8 @@ object España : Pais{
     override val region = "Europe"
     override val population = 46438022
     override val borders = listOf<String>("AND","FRA","GIB","PRT","MAR")
-    override val languages = listOf<Lenguages>(Lenguages("Spanish"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("EU","European Union"))
+    override val languages = listOf<Language>(Language("Spanish"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("EU","European Union"))
 }
 
 object Panama : Pais{
@@ -99,8 +99,8 @@ object Panama : Pais{
     override val region = "Americas"
     override val population = 3814672
     override val borders = listOf<String>("COL","CRI")
-    override val languages = listOf<Lenguages>(Lenguages("Spanish"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("CAIS","Central American Integration System"))
+    override val languages = listOf<Language>(Language("Spanish"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("CAIS","Central American Integration System"))
 }
 
 object China : Pais{
@@ -110,8 +110,8 @@ object China : Pais{
     override val region = "Asia"
     override val population = 1377422166
     override val borders = listOf<String>("AFG", "BTN", "MMR", "HKG", "IND", "KAZ", "PRK", "KGZ", "LAO", "MAC", "MNG", "PAK", "RUS", "TJK", "VNM")
-    override val languages = listOf<Lenguages>(Lenguages("Chinese"))
-    override val regionalBlocs =  listOf<regionalBlocs>(regionalBlocs("EU","European Union"))
+    override val languages: List<Language> = listOf<Language>(Language("Chinese"))
+    override val regionalBlocs =  listOf<RegionalBloc>(RegionalBloc("EU","European Union"))
 }
 
 class AdapterCountry(country: Country) : Pais {
@@ -121,4 +121,6 @@ class AdapterCountry(country: Country) : Pais {
     override val region: String=country.region
     override val population: Int=country.population
     override val borders: List<String> = country.borders
+    override val languages: List<Language> = country.languages
+    override val regionalBlocs: List<RegionalBloc> = country.regionalBlocs
 }
---
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/RegionalBloc.kt
@@ -0,0 +1,3 @@
+package ar.edu.unahur.obj2.impostoresPaises
+
+class RegionalBloc (val acronym: String, val name: String)
