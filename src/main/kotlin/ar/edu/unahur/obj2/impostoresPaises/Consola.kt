aaf06d35383936ef645bb06c1447aec9f2b741f4
agustin otero <popal9193@gmail.com>
2020-11-23 12:29:17 -0300

    Consola - minor fix

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Consola.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Consola.kt
@@ -27,12 +27,12 @@ object Programa{
         entradaSalida.escribirLinea("Menu principal")
         entradaSalida.escribirLinea("Ingrese numero de operacion:")
         entradaSalida.escribirLinea("1 - Info de un pais.")
-        entradaSalida. escribirLinea("2 - Paises limitrofes.")
-        entradaSalida. escribirLinea("3 - Paises que pueden dialogar sin interprete.")
-        entradaSalida. escribirLinea("4 - Paises potenciales aliados.")
-        entradaSalida. escribirLinea("5 - Los 5 paises con mayor poblacion.")
-        entradaSalida. escribirLinea("6 - Contiente mas poblado.")
-        entradaSalida. escribirLinea("0 - Salir")
+        entradaSalida.escribirLinea("2 - Paises limitrofes.")
+        entradaSalida.escribirLinea("3 - Paises que pueden dialogar sin interprete.")
+        entradaSalida.escribirLinea("4 - Paises potenciales aliados.")
+        entradaSalida.escribirLinea("5 - Los 5 paises con mayor poblacion.")
+        entradaSalida.escribirLinea("6 - Contiente mas poblado.")
+        entradaSalida.escribirLinea("0 - Salir")
         lineaEnBlanco()
     }
 
@@ -53,21 +53,21 @@ object Programa{
             }
         }
         catch (e: Exception){ // si no se ingresa un numero sale por aca
-            entradaSalida. escribirLinea("El menu se maneja solo con numeros del 0 al 6")
+            entradaSalida.escribirLinea("El menu se maneja solo con numeros del 0 al 6")
         }
         finally {
-            entradaSalida. escribirLinea("¡Gracias por usar nuestro programa!")
+            entradaSalida.escribirLinea("¡Gracias por usar nuestro programa!")
         }
     }
 
     fun primeraOpcion() {
-        entradaSalida. escribirLinea("Ingrese el nombre de un pais")
+        entradaSalida.escribirLinea("Ingrese el nombre de un pais")
         try { // intenta buscar el pais e imprimir la informacion
             val pais = ObsvApi.encontrarPais(entradaSalida.leerLinea())
-            entradaSalida. escribirLinea("El pais ${pais.name} tiene ${pais.population} habitantes y habla ${pais.languages}")
+            entradaSalida.escribirLinea("El pais ${pais.name} tiene ${pais.population} habitantes y habla ${pais.languages}")
         }
         catch (e: Exception){ // si hay error lo dice
-            entradaSalida. escribirLinea("No existe tal pais, volvemos al menu.")
+            entradaSalida.escribirLinea("No existe tal pais, volvemos al menu.")
         }
         finally { // muestra de nuevo el menu y espera un numero en opcionMenu
             volverAlMenu()
@@ -75,17 +75,17 @@ object Programa{
     }
 
     fun opcionDosTresCuatro(){
-        entradaSalida. escribirLinea("Ingrese el nombre de un pais")
+        entradaSalida.escribirLinea("Ingrese el nombre de un pais")
         val pais1  = entradaSalida.leerLinea()
-        entradaSalida. escribirLinea("Ingrese el nombre de otro pais")
+        entradaSalida.escribirLinea("Ingrese el nombre de otro pais")
         var pais2 =entradaSalida. leerLinea()
         while (pais2 == pais1){ // comprobacion de que no son iguales
-            entradaSalida. escribirLinea("No se puede comparar un pais con sigo mismo.")
-            entradaSalida. escribirLinea("Ingrese el nombre de otro pais.")
+            entradaSalida.escribirLinea("No se puede comparar un pais con sigo mismo.")
+            entradaSalida.escribirLinea("Ingrese el nombre de otro pais.")
             pais2 = entradaSalida.leerLinea()
         }
         try {// intenta hacer la operacion
-            entradaSalida. escribirLinea(  // aca hice magia , y con el mismo metodo , meti las 3 opciones.
+            entradaSalida.escribirLinea(  // aca hice magia , y con el mismo metodo , meti las 3 opciones.
                 "los paises $pais1 y $pais2 "+
                         when (opcionMenu) { // segun opcion del menu es la respuesta
                             2 -> if(ObsvApi.sonLimitrofes(pais1,pais2)){""}else{"no"}+ " son limitrofes."+"\n"
@@ -105,23 +105,23 @@ object Programa{
     fun quintaOpcion() {
         val listaPaises = ObsvApi.cincoPaisesConMayorPoblacion()
         entradaSalida.escribirLinea("Los 5 paises con mayor poblacion son:")
-        entradaSalida. escribirLinea(listaPaises[0])
-        entradaSalida. escribirLinea(listaPaises[1])
-        entradaSalida. escribirLinea(listaPaises[2])
-        entradaSalida. escribirLinea(listaPaises[3])
-        entradaSalida. escribirLinea(listaPaises[4])
+        entradaSalida.escribirLinea(listaPaises[0])
+        entradaSalida.escribirLinea(listaPaises[1])
+        entradaSalida.escribirLinea(listaPaises[2])
+        entradaSalida.escribirLinea(listaPaises[3])
+        entradaSalida.escribirLinea(listaPaises[4])
         volverAlMenu()
     }
 
     fun sextaOpcion() {
-        entradaSalida. escribirLinea("El continenete mas poblado es:")
-        entradaSalida. escribirLinea(ObsvApi.continenteMasPoblado())
+        entradaSalida.escribirLinea("El continenete mas poblado es:")
+        entradaSalida.escribirLinea(ObsvApi.continenteMasPoblado())
         volverAlMenu()
     }
 
     fun errorReingresoMenu() {
-        entradaSalida. escribirLinea("Error al seleccionar opcion.")
-        entradaSalida. escribirLinea("Ingrese nuevamente.")
+        entradaSalida.escribirLinea("Error al seleccionar opcion.")
+        entradaSalida.escribirLinea("Ingrese nuevamente.")
         ingresoOpcionMenu()
     }
 
