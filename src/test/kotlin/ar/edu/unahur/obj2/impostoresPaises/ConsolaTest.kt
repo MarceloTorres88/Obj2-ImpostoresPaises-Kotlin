342c881341e7668ba21b504deaa78209c7770801
MarceloTorres88 <russo1988torres@gmail.com>
2020-11-23 13:15:13 -0300

    Consola test terminado

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/ConsolaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/ConsolaTest.kt
@@ -43,7 +43,30 @@ class ConsolaTest: DescribeSpec ({
             verify {
                 consola.escribirLinea("Ingrese el nombre de un pais")
                 consola.escribirLinea("Ingrese el nombre de otro pais")
+                //en el caso de nuestra opcion 2, siempre hace un enter despues de cada linea
                 consola.escribirLinea("los paises Argentina y Brazil  son limitrofes."+"\n")
+            }
+        }
+
+        it("Opcion 2 se ingreso mal un pais"){
+            every { consola.leerLinea() } returns "2" andThen "argola" andThen "0"
+
+            Programa.iniciar()
+
+            verify {
+                consola.escribirLinea("Ingrese el nombre de un pais")
+                consola.escribirLinea("Se ingreso mal un pais, volvemos al menu.")
+            }
+        }
+
+        it("Continente mas poblado"){
+            every { consola.leerLinea() } returns "6" andThen "0"
+
+            Programa.iniciar()
+
+            verify {
+                consola.escribirLinea("El continenete mas poblado es:")
+                consola.escribirLinea("Asia")
             }
         }
 
