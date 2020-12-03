aa08ecb3209d39659634cd45177e9674338e38d5
agustin otero <popal9193@gmail.com>
2020-12-03 13:28:58 -0300

    buscar casos X Pais X fecha. solo casos felices. enprolijado y comentado el funcionamiento.

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/RestCountriesAPI.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/RestCountriesAPI.kt
@@ -11,7 +11,7 @@ import java.lang.reflect.Type
 // Pueden mirar cómo está hecho si les da curiosidad,
 // pero no pueden cambiar absolutamente nada de este archivo.
 
-abstract class Apis {
+abstract class Apis { // aca adapte como superclase lo que hace que se comunique con la api y lo transforme en algo que podamos usar
     abstract val urlBase : String
     val client = OkHttpClient()
     val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
@@ -34,14 +34,12 @@ abstract class Apis {
 
 open class RestCountriesAPI  : Apis() {
   override val urlBase = "https://restcountries.eu/rest/v2"
-//  private val client = OkHttpClient()
-//  private val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
-  private val countriesAdapter = crearAdapter<List<Country>>(
+  private val countriesAdapter = crearAdapter<List<Country>>( // esto es para lista de contryes
     Types.newParameterizedType(List::class.java, Country::class.java)
   )
 
-  private val countryAdapter = crearAdapter<Country>(Country::class.java)
+  private val countryAdapter = crearAdapter<Country>(Country::class.java) // esto es para un solo contry
 
   fun todosLosPaises() = obtenerRespuesta("/all", countriesAdapter)!!
 
@@ -53,21 +51,6 @@ open class RestCountriesAPI  : Apis() {
       obtenerRespuesta("/alpha/${alpha3Code}", countryAdapter),
       { "No se encontró ningún país con el código $alpha3Code" }
     )
-
-//  private fun <T> obtenerRespuesta(ruta: String, adapter: JsonAdapter<T>): T? {
-//    val response = client.newCall(crearRequest(urlBase + ruta)).execute()
-//    return if (response.isSuccessful) { adapter.fromJson(response.body!!.source())!! } else { null }
-//  }
-//
-//  private fun crearRequest(url: String): Request {
-//    return Request.Builder()
-//      .url(url)
-//      .build()
-//  }
-//
-//  private fun <T> crearAdapter(type: Type): JsonAdapter<T> {
-//    return moshi.adapter(type)
-//  }
 }
 
 // Tomamos solamente un subconjunto de la información que da la API.
@@ -99,37 +82,26 @@ data class RegionalBloc(
 class CovidApi : Apis() {
     override val urlBase: String = "https://api.covid19api.com/"
 
-    private val covidAdapter = crearAdapter<CovidCountry>(CovidCountry::class.java) // este es para 1 solo
+    private val infoCovidAdapter = crearAdapter<CovidCountry>(CovidCountry::class.java) // este es para 1 solo contry covid
 
-    private val covidsAdapter = crearAdapter<List<CovidCountry>>( // este es para muchos
+    private val infoCovidVariosAdapter = crearAdapter<List<CovidCountry>>( // este es para muchos country covids
         Types.newParameterizedType(List::class.java, CovidCountry::class.java)
     )
 
-//    fun todosLosPaises() = obtenerRespuesta("countries", covidAdapter)!!
-
-
-//    fun buscarPaisesPorNombre(nombre: String) =
-//        obtenerRespuesta("/name/${nombre}", covidAdapter)//.orEmpty()
-
-//    https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
-
-//    fun paisConCodigo(alpha3Code: String) =
-//        checkNotNull(
-//            obtenerRespuesta("/alpha/${alpha3Code}", covidAdapter),
-//            { "No se encontró ningún país con el código $alpha3Code" }
-//        )
-
 //    https://api.covid19api.com/country/argentina/status/confirmed?from=2020-09-08T00:00:00Z&to=2020-09-09T00:00:00Z
+    // para probar en navegador el enlace que quiero que devuelva prueba()
 
-    fun prueba () = obtenerRespuesta("country/argentina/status/confirmed?from=2020-09-08T00:00:00Z&to=2020-09-09T00:00:00Z", covidsAdapter)
+    fun prueba () = obtenerRespuesta("country/argentina/status/confirmed?from=2020-09-08T00:00:00Z&to=2020-09-09T00:00:00Z", infoCovidVariosAdapter)
+    // prueba de que realmente entrega lo que quiero
 
     fun buscarCasosPorPaisPorFecha(pais : String ,inicio: String ): List<CovidCountry> { //fecha en este formato : 2020-03-01T00:00:00Z (año,mes,dia)
         val fin = fechaMas1Dia(inicio)
-        return obtenerRespuesta("country/${pais}/status/confirmed?from=${inicio}&to=${fin}", covidsAdapter).orEmpty()
+        return obtenerRespuesta("country/${pais}/status/confirmed?from=${inicio}&to=${fin}", infoCovidVariosAdapter).orEmpty()
     }
 
-    fun fechaMas1Dia(inicio: String) = //2020-03-01T00:00:00Z (el dia es el del medio) y solo caso feliz (que no cambia el mes)
+    fun fechaMas1Dia(inicio: String) = //2020-03-01T00:00:00Z y solo caso feliz (que no cambia el mes no el primer digito del dia)
         inicio.substring(0,9)+inicio[9].plus(1)+inicio.substring(10,20)
+        // corto el string varias veces y en la posicion del dia (segundo digito) le sumo 1
 
 
 }
