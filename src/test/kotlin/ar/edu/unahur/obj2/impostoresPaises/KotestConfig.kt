3b997b21d378ffff8dff1f12047c42c87ca55ee6
MarceloTorres88 <russo1988torres@gmail.com>
2021-04-18 13:19:58 -0300

    git

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/KotestConfig.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/KotestConfig.kt
@@ -3,6 +3,7 @@ package ar.edu.unahur.obj2.impostoresPaises
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.spec.IsolationMode
 
+
 class KotestConfig: AbstractProjectConfig() {
   // Modificamos esta configuración para que se creen nuevos objetos para cada `it`.
   override val isolationMode = IsolationMode.InstancePerLeaf
